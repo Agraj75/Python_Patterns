c55cd51bde745d449621287567af97d5da44dca6
Agraj75 <hukeagraj75@gmail.com>
2025-03-22 21:50:27 +0530

    Added right-angled triangle pattern program

--- RightAngledTrianglePattern.py
+++ RightAngledTrianglePattern.py
@@ -1,5 +1,7 @@
 n=7
 for i in range(n):
-    for j in range(i+1):
-        print("* ",end='')
+    for j in range(i,n-1):
+        print(" ",end = " ")
+    for k in range(i+1):
+        print("* ",end = "")
     print()
